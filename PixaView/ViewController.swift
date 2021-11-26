7221870fd5f24baf8e1db455678eca279d157dbe
Dmitry Ihnatiev <straenger@gmail.com>
2021-11-26 21:10:27 +0200

    Initial Commit

---
+++ PixaView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PixaView
+//
+//  Created by Дмитрий Игнатьев on 26.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
