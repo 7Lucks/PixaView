bf7f2a811c2bb88c49b3d1a2641253135c04e02c
Dmitry Ihnatiev <straenger@gmail.com>
2021-11-28 00:57:20 +0200

    added json decoder and structs for receiving data from Pixabay

--- PixaView/Controller/PixaViewController.swift
+++ PixaView/Controller/PixaViewController.swift
@@ -12,31 +12,61 @@ class PixaViewController: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView!
     //MARK: - end of Outlets
     
+    //MARK:  Properties -
+    let urlStr = "https://pixabay.com/api/?key=16834549-9bf1a2a9f7bfa54e36404be81&q=yellow+flowers&image_type=photo"  // https://pixabay.com/api/
+    
+    
+    //MARK: - end of Properties
+    
     //MARK:  viewDidLoad -
     override func viewDidLoad() {
         super.viewDidLoad()
+
+//        self.collectionView.register(UINib(nibName: "PixaViewCell", bundle: nil), forCellWithReuseIdentifier: "PixaViewCell") //assign to cell the Xib file
+//        self.collectionView.dataSource = self //the cell need to understand where to get the filling from. add protocol to extension - UICollectionViewDataSource
+//        self.collectionView.delegate = self //delegate with collectionView. Subscribe to UICollectionViewDelegate as delegate
+        fetchPics()
         
-        self.collectionView.register(UINib(nibName: "PixaViewCell", bundle: nil), forCellWithReuseIdentifier: "PixaViewCell") //assign to cell the Xib file
-        self.collectionView.dataSource = self //the cell need to understand where to get the filling from. add protocol to extension - UICollectionViewDataSource
-        self.collectionView.delegate = self //delegate with collectionView. Subscribe to UICollectionViewDelegate as delegate
     }
     //MARK: - End of viewDidLoad
 
+    //MARK: Methods -
+    func fetchPics(){
+        guard let url = URL(string: urlStr) else{return}
+        let task = URLSession.shared.dataTask(with: url){ data, response, error in
+            guard let data = data, let response = response, error == nil else {
+                return
+            }
+           // print("have some data") +
+            //do catch for JSON decoder
+            do{
+                let jsonResults = try JSONDecoder().decode(APIResponse.self, from: data)
+                print(jsonResults.total)
+            }catch{
+                print("Here is some ERR - \(error)")
+                
+            }
+        }
+        task.resume()
+    }
+    
+    //MARK: - End of Methods
+    
     
 } // end of PixaViewController class
 
 //MARK: Extensions -
-extension PixaViewController:UICollectionViewDataSource, UICollectionViewDelegate{
-    // number of cells
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 0
-    }
-        // what type of the cells
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        <#code#>
-    }
-    
-}
+//extension PixaViewController:UICollectionViewDataSource, UICollectionViewDelegate{
+//    // number of cells
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return 0
+//    }
+//    // what type of the cells
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        <#code#>
+//    }
+//    
+//}
 
 
 
---
+++ PixaView/Model/ApiResponse.swift
@@ -0,0 +1,21 @@
+//
+//  ApiResponse.swift
+//  PixaView
+//
+//  Created by Дмитрий Игнатьев on 28.11.2021.
+//
+
+import UIKit
+
+
+struct APIResponse: Codable{
+    let total: Int
+    let totalHits: Int
+    let hits: [Hits]
+}
+
+struct Hits: Codable{
+    let id: Int
+    let imageURL: String
+}
+
