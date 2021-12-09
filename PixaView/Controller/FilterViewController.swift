df53422c299a8fbb60ff5676f14e872d89b1e04c
Dmitry Ihnatiev <straenger@gmail.com>
2021-12-09 15:34:57 +0200

    added ui table view cell as Filter

--- PixaView/Controller/FilterViewController.swift
+++ PixaView/Controller/FilterViewController.swift
@@ -7,12 +7,35 @@
 
 import UIKit
 
-class FilterViewController: UIViewController {
+class FilterViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+   
+    let tableView = UITableView()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.addSubview(tableView)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }//end of viewDidLoad
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
     }
     
-}
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "cell\(indexPath.row + 1)"
+        
+        return cell
+    }
+    
+}// end of FilterViewController
 
