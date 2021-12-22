5c63516a6420f50588293d75b7c11bca36f43710
Dmitry Ihnatiev <straenger@gmail.com>
2021-12-22 10:03:35 +0200

    try to present downloaded images to cells at main VC

--- PixaView/Controller/PixaViewController.swift
+++ PixaView/Controller/PixaViewController.swift
@@ -12,11 +12,11 @@ class PixaViewController: UIViewController {
     //MARK: Outlets -
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var sortButtonOutlet: UIButton!
-        //MARK: - end of Outlets
+    //MARK: - end of Outlets
     
     //MARK:  Properties -
-
-  //  let urlStr = "https://pixabay.com/api/?key=16834549-9bf1a2a9f7bfa54e36404be81&q=china&per_page=100&image_type=photo" // https://pixabay.com/api/
+    
+    //  let urlStr = "https://pixabay.com/api/?key=16834549-9bf1a2a9f7bfa54e36404be81&q=china&per_page=100&image_type=photo" // https://pixabay.com/api/
     var hitsRESULT: [Hits] = [] //array from json
     var popularLastetstButton = PopularLastestButton()
     var enumValue = ""
@@ -30,55 +30,56 @@ class PixaViewController: UIViewController {
         self.collectionView.register(UINib(nibName: PixaViewCell.cellIdentifier, bundle: nil), forCellWithReuseIdentifier: PixaViewCell.cellIdentifier) //assign to cell the Xib file
         self.collectionView.dataSource = self //the cell need to understand where to get the filling from. add protocol to extension - UICollectionViewDataSource
         self.collectionView.delegate = self //delegate with collectionView. Subscribe to UICollectionViewDelegate as delegate
-        fetch(order: .popular, filterCategory: [.backgrounds], currentPage: 1)
+        //fetch(order: .popular, filterCategory: [.backgrounds], currentPage: 1)
+        fetch()
         droppedMunu()
         setImage()
-    
+        
     }
     //MARK: - End of viewDidLoad
     
     //MARK:  dropped button-
     
- func droppedMunu(){
-    let droppedSortMenu = UIMenu(title:"PixaSort", children: [
-        UIAction(title: "Popular", image: UIImage(systemName: "tray.full"), handler: {(_) in
-            self.popularLastetstButton.pixaGallerySort(1, order: .popular )
-            self.didSelectSortingStrategy(order: .popular)
-        }),
-        UIAction(title: "Lastest", image: UIImage(systemName: "star.circle"), handler: {(_) in
-            self.popularLastetstButton.pixaGallerySort(1, order: .latest)
-            self.didSelectSortingStrategy(order: .latest)
-        })
+    func droppedMunu(){
+        let droppedSortMenu = UIMenu(title:"PixaSort", children: [
+            UIAction(title: "Popular", image: UIImage(systemName: "tray.full"), handler: {(_) in
+                self.popularLastetstButton.pixaGallerySort(1, order: .popular )
+                self.didSelectSortingStrategy(order: .popular)
+            }),
+            UIAction(title: "Lastest", image: UIImage(systemName: "star.circle"), handler: {(_) in
+                self.popularLastetstButton.pixaGallerySort(1, order: .latest)
+                self.didSelectSortingStrategy(order: .latest)
+            })
         ])
-    self.sortButtonOutlet.menu = droppedSortMenu
-    self.sortButtonOutlet.showsMenuAsPrimaryAction = true
-    
+        self.sortButtonOutlet.menu = droppedSortMenu
+        self.sortButtonOutlet.showsMenuAsPrimaryAction = true
+        
     }
-
+    
     func didSelectSortingStrategy( order: PopularLastestButton.Order){
-//        var urlString = ""
-//        switch order {
-//        case .latest:
-//            urlString = "https://pixabay.com/api/?key=\(myKeyId)&editors_choice=true&per_page=150&order=latest"
-//        case .popular:
-//            urlString = "https://pixabay.com/api/?key=\(myKeyId)&editors_choice=true&per_page=150&order=popular"
-//
-//        } //end of swith
-//        changedUrl(newURL: URL(string: urlString)!)
+        //        var urlString = ""
+        //        switch order {
+        //        case .latest:
+        //            urlString = "https://pixabay.com/api/?key=\(myKeyId)&editors_choice=true&per_page=150&order=latest"
+        //        case .popular:
+        //            urlString = "https://pixabay.com/api/?key=\(myKeyId)&editors_choice=true&per_page=150&order=popular"
+        //
+        //        } //end of swith
+        //        changedUrl(newURL: URL(string: urlString)!)
     }
     
     
     func didSelectCategory(category: [Categories]){
-//
-//        let filtredURL = "https://pixabay.com/api/?key=16834549-9bf1a2a9f7bfa54e36404be81&q=\(enumValue)&per_page=100&image_type=photo"
-//        print("selected \(enumValue) category")
-//        changedUrl(newURL: URL(string: filtredURL)!)
-//
-   }
+        //
+        //        let filtredURL = "https://pixabay.com/api/?key=16834549-9bf1a2a9f7bfa54e36404be81&q=\(enumValue)&per_page=100&image_type=photo"
+        //        print("selected \(enumValue) category")
+        //        changedUrl(newURL: URL(string: filtredURL)!)
+        //
+    }
     
     
     //MARK: - end of dreopped button
- //MARK: - actions
+    //MARK: - actions
     @IBAction func filterButtonDidTap(_ sender: UIButton) {
         guard let filterVC = storyboard?.instantiateViewController(withIdentifier: "FilterViewControllerID") else{return}
         //present(filterVC, animated: true, completion: nil)
@@ -88,12 +89,13 @@ class PixaViewController: UIViewController {
         
         let navVC = UINavigationController(rootViewController: filterVC)
         present(navVC, animated: true, completion: nil)
-
+        
     }
     
     
     //MARK: Methods -
-    func fetch(order : PopularLastestButton.Order, filterCategory:[Categories],currentPage:Int){
+    // func fetch(order : PopularLastestButton.Order, filterCategory:[Categories],currentPage:Int){
+    func fetch(){
         let URLSession = URLSession.shared
         let service: HTTPService = HTTPService(with: URLSessionHttpClient(session: URLSession))
         service.fetchPics(order: .popular, filterCategory: [.backgrounds], currentPage: 1) { fetchedHits  in
@@ -104,42 +106,47 @@ class PixaViewController: UIViewController {
                 self.collectionView.reloadData()
             } // dispatch
         }
+        print ("somw - \(hitsRESULT)")
     } // end of fetchPics
     
     
     // MARK: QUESTION : setImage to cell -
     func setImage(){
-        
-        
-        
         //guard let url = URL(string: urlString) else{
-      //            return
-      //        }
-      //        let taskSetImage = URLSession.shared.dataTask(with: url){data, _, error in
-      //            guard let data = data, error == nil else{
-      //                return
-      //            }
-      //            DispatchQueue.main.async {
-      //                let image = UIImage(data: data)
-      //                self.pixaImageOutlet.image = image
-      //                self.activityIndicator.stopAnimating()
-      //            }
-      //        }
-      //        taskSetImage.resume()
-    let pixaViewCell = PixaViewCell()
-        let fetchedData = fetch(order: .popular, filterCategory: [.backgrounds], currentPage: 1)
+        //            return
+        //        }
+        //        let taskSetImage = URLSession.shared.dataTask(with: url){data, _, error in
+        //            guard let data = data, error == nil else{
+        //                return
+        //            }
+        //            DispatchQueue.main.async {
+        //                let image = UIImage(data: data)
+        //                self.pixaImageOutlet.image = image
+        //                self.activityIndicator.stopAnimating()
+        //            }
+        //        }
+        //        taskSetImage.resume()
+        let pixaViewCell = PixaViewCell()
+        // let fetchedData = fetch(order: .popular, filterCategory: [.backgrounds], currentPage: 1)
         
         DispatchQueue.main.async {
-        
-                
-//            pixaViewCell.pixaImageOutlet.image =
-               // pixaViewCell.activityIndicator.stopAnimating()
-        }
             
+            
+            //            pixaViewCell.pixaImageOutlet.image =
+            // pixaViewCell.activityIndicator.stopAnimating()
         }
         
-   
-
+    }
+    
+    //    func fetchHits(order : PopularLastestButton.Order , filterCategory:[Categories], currentPage:Int) {
+    //
+    //      HTTPService(with:
+    //                self.hits.append(contentsOf:post)
+    //                self.totalHits = totalHits
+    //            }, order: order, selectedCategory: selectedCategory,currentPage: currentPage)
+    //
+    //    }
+    
     //MARK: - End of Methods
     
 } // end of PixaViewController class
@@ -154,22 +161,23 @@ extension PixaViewController:UICollectionViewDataSource, UICollectionViewDelegat
     // type of the cells
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        let imageURLString = hitsRESULT[indexPath.item].webformatURL  //image from json to cell
-        
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PixaViewCell.cellIdentifier, for: indexPath) as? PixaViewCell else{
-            return PixaViewCell()
+            return PixaViewCell()}
+        
+        let hitsTags = hitsRESULT[indexPath.item].tags
+        
+        let url = URL(string: hitsRESULT[indexPath.row].webformatURL)!
+        ImageLoader(with: URLSessionHttpClient(session: URLSession.shared)).loadPics(from: url) { (image) in
+            DispatchQueue.main.async {
+                guard collectionView.indexPath(for: cell) == indexPath else {return }
+                
+                cell.pixaImageOutlet.image = image
+                cell.tagsLabelOutlet.text = hitsTags
+            }
         }
-        // cell.backgroundColor = .systemGray
-        cell.tagsLabelOutlet.text = hitsRESULT[indexPath.row].tags
-        // MARK: QUESTION : config cell -
-     //   cell.configure(with: imageURLString)
-        cell.cellPixaConfig()
         return cell
     } // end of collectionView
     
-    
-    
-    // MARK: QUESTION : при перевороте устройства варианты указать чере свитч?
     // cell cizes  // as diasplay // vertical
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.width )  // need to check
@@ -211,7 +219,7 @@ extension PixaViewController:UICollectionViewDataSource, UICollectionViewDelegat
         
         self.navigationController?.pushViewController(vc, animated: true)
     }
-
+    
 }// end of Extensions
 
 
--- PixaView/Model/NetworkService.swift
+++ PixaView/Model/NetworkService.swift
@@ -9,7 +9,7 @@ import TestFramework
 
 let myKeyId = "16834549-9bf1a2a9f7bfa54e36404be81"
 
- enum Categories: String, CaseIterable{
+enum Categories: String, CaseIterable{
     case backgrounds
     case fashion
     case nature
@@ -35,11 +35,11 @@ let myKeyId = "16834549-9bf1a2a9f7bfa54e36404be81"
 
 public class HTTPService{
     let session: HTTPClient
-
-   public init(with session: HTTPClient) {
+    
+    public init(with session: HTTPClient) {
         self.session = session
     }
-
+    //MARK:  Methods -
     func fetchPics(order : PopularLastestButton.Order ,filterCategory:[Categories],currentPage:Int, completion: @escaping ([Hits]) -> ()) {
         
         let filterCategory:[Categories] = Categories.allCases
@@ -54,23 +54,52 @@ public class HTTPService{
             "order": order,
             "category": filterCategory
         ].map({ URLQueryItem(name: $0, value: "\($1)")})
-     
+        
         session.get(from: urlComponents.url!) { (result) in
             switch result {
             case let .success(data, response):
                 completion((try? JSONDecoder().decode(ImageAPIResponse.self, from: data).hits) ?? [])
             case let .failure(error):
                 break
+            }//end of switch
+        }// end of session
+        print ("")
+    } // end of fetchPics
+} // end of HTTPService
+
+
+//MARK: ImageLoader -
+
+public struct ImageLoader {
+    
+    public let session: HTTPClient
+    
+    public init(with session: HTTPClient) {
+        self.session = session
+    }
+    
+    //MARK: PictureLoader-
+    
+    public  func loadPics(from url: URL, completion: @escaping (UIImage) -> ()) {
+        session.get(from: url) { (result) in
+            switch result {
+            case let .success(data, _):
+                completion(UIImage(data: data)!)
+            case let .failure(error):
+                break
             }
+            
         }
     }
-}
+} // end of ImageLoader
+
+
+//MARK:  Extensions -
 extension URLSession: NetworkSession{
     
     public func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> NetworkRequest {
         let datatask = dataTask(with: URLRequest(url: url), completionHandler: completionHandler)
         return datatask
     }
-    
-     
 }
+//MARK: - end of Extensions
